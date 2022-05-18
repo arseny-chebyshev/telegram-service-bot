d71a7334432e327fbf389f9a08256b44513be37b
arseny-chebyshev <arseny.chebyshev@gmail.com>
2022-05-18 15:35:34 +0500

    deprecate test functions

--- settings.py
+++ settings.py
@@ -4,8 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 bot_token = os.getenv('BOT_TOKEN')
 admin_id = os.getenv('ADMIN_ID')
-apple_url = os.getenv('APPLE')
-peach_url = os.getenv('PEACH')
 db_address = os.getenv('DB_ADDRESS')
 pg_user = os.getenv('PG_USER')
 pg_pass = os.getenv('PG_PASS')
